be5d417150491b6102930240b0748c84037bb1cf
Shestakov Matvey <mse25019@gmail.com>
2026-03-21 10:10:02 +0500

    feat(model): add Set<Role>

--- src/main/java/com/bank/account/model/User.java
+++ src/main/java/com/bank/account/model/User.java
@@ -2,6 +2,14 @@ package com.bank.account.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name="users")
@@ -11,7 +19,7 @@ import lombok.*;
 @Getter @Setter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @ToString(onlyExplicitlyIncluded = true)
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     @EqualsAndHashCode.Include
@@ -32,4 +40,46 @@ public class User {
 
     @Column(nullable = false)
     private String lastName;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @Column(nullable = false)
+    private Set<Role> roles;
+
+    @Column(name = "created_at")
+    private LocalDateTime created_at;
+
+    @PrePersist
+    protected void onCreate() {
+        created_at = LocalDateTime.now();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return getRoles();
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
