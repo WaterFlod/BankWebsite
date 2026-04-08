4df213c496f0046e2e9bf7d6042d3aeabb77e5d0
Shestakov Matvey <mse25019@gmail.com>
2026-04-08 08:46:07 +0500

    refactor: remove unuseable file

--- src/main/java/com/bank/account/dto/AuthResponse.java
+++
@@ -1,4 +0,0 @@
-package com.bank.account.dto;
-
-public record AuthResponse(String token, String email) {
-}
--- src/main/java/com/bank/account/repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bank.account.repository;
-
-import com.bank.account.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository  extends JpaRepository<Role, Long> {
-}
