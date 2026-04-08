dbbee7cb848f8f9599c368922090a904c49482b0
Shestakov Matvey <mse25019@gmail.com>
2026-04-08 09:03:50 +0500

    feat(repository): update UserRepository
    - add findByPhoneNumber
    - add existsByEmail
    - add existsByPhoneNumber

--- src/main/java/com/bank/account/repository/UserRepository.java
+++ src/main/java/com/bank/account/repository/UserRepository.java
@@ -9,5 +9,7 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
     Optional<User> findByEmail(String email);
-    boolean exsistsByEmail(String email);
+    Optional<User> findByPhoneNumber(String PhoneNumber);
+    boolean existsByEmail(String email);
+    boolean existsByPhoneNumber(String PhoneNumber);
 }
